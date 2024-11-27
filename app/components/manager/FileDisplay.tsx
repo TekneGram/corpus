e5f91db64e1d8a111c39b489cd966daa29b00d12
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-28 00:05:37 +0900

    style: hide the scrollbar in subcorpus files display

--- app/components/manager/FileDisplay.tsx
+++ app/components/manager/FileDisplay.tsx
@@ -54,17 +54,34 @@ const FileDisplay:React.FC<FileDisplayProps> = ({ subCorpusFiles }) => {
     };
 
     /**
+     * Change the name of the subCorpus
+     */
+    const handleChangeName = () => {
+        // TODO
+        alert("You will soon be able to change the name of the subcorpus")
+    }
+
+    /**
      * Adding new files
      */
     const handleAddFiles = () => {
         // TODO
+        alert("You will be able to add files soon!")
+    }
+
+    /**
+     * Deleting files that are no longer needed
+     */
+    const handleDeleteFile = () => {
+        // TODO
+        alert("This file will be deleted!");
     }
 
     return (
         <div>
             <div className="group-name-display">
                 {/* Subcorpus name area. Can click to change the name of the subcorpus */}
-                <div>{subCorpusFiles.subCorpus.group_name}</div>
+                <div onClick={handleChangeName}>{subCorpusFiles.subCorpus.group_name}</div>
                 <div className="group-name-display-file-number">
                     <div>{subCorpusFiles.corpusFiles.length} {subCorpusFiles.corpusFiles.length === 1 ? 'file' : 'files'}</div>
                     <div onClick={handleAddFiles}>Add files</div>
@@ -82,7 +99,7 @@ const FileDisplay:React.FC<FileDisplayProps> = ({ subCorpusFiles }) => {
                             <div className='file-name' onClick={() => selectFile(corpusFile.id)}>
                                 <div>{corpusFile.file_name}</div>
                                 {
-                                    fileNameSelected.find(file => file.fileId === corpusFile.id)?.selected ? <div>Delete File</div> : ''
+                                    fileNameSelected.find(file => file.fileId === corpusFile.id)?.selected ? <div onClick={handleDeleteFile}>Delete File</div> : ''
                                 }
                             </div>
 
