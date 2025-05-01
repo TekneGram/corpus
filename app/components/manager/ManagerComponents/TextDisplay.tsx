8bb66784fdfa7c6a1803c58fb8a051c859664354
Daniel Parsons <parsonsdwp@gmail.com>
2025-05-01 21:34:10 +0900

    chore: add imports to TextDisplay component

--- app/components/manager/ManagerComponents/TextDisplay.tsx
+++ app/components/manager/ManagerComponents/TextDisplay.tsx
@@ -1,5 +1,18 @@
+// CSS
+import '@/manager.css';
 
-const TextDisplay = () => {
+// import APIs
+
+import { getCorpusFileText } from '@/app/api/manageCorpus';
+
+// Context and state management
+import { useState } from 'react';
+import { useCorpusMetaData, useCorpusDispatch } from '@/app/context/CorpusContext';
+
+// Child components
+import { toast } from 'react-toastify';
+
+const TextDisplay: React.FC = () => {
 
     return (
         <div className='text-display-area'>
