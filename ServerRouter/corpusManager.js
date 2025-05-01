4cf254e3d80cdae516e324863b86e39c1826b2a0
Daniel Parsons <parsonsdwp@gmail.com>
2025-05-01 21:41:43 +0900

    chore: add backend router and controller to get text

--- ServerRouter/corpusManager.js
+++ ServerRouter/corpusManager.js
@@ -11,6 +11,7 @@ const {
     uploadFileContent,
     deleteCorpusFile,
     deleteSubcorpus,
+    getFileText,
 } = require('../ServerControllers/corpusManagerController.js');
 
 
@@ -28,5 +29,6 @@ router.patch('/groups/:groupId', patchCorpusGroupName); // edit a group name ass
 router.post('/groups/:groupId/file', uploadFileContent); // add a file associated with a particular group id.
 router.delete('/files/:fileId', deleteCorpusFile); // delete a file given its ID. Will also delete other data associated with it.
 router.delete('/groups/:groupId', deleteSubcorpus); // delete a corpus group given its ID. Will also delete other data associated with the subcorpus.
+router.get('/text/:fileId', getFileText);
 
 module.exports = router;
