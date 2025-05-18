327c3aaa1fd0c00a44a175dc66d65cb95e67d36e
Daniel Parsons <parsonsdwp@gmail.com>
2025-05-18 10:38:38 +0900

    feat: add status checks when adding a new file to the corpus

--- ServerRouter/corpusManager.js
+++ ServerRouter/corpusManager.js
@@ -11,6 +11,7 @@ const {
     uploadFileContent,
     deleteCorpusFile,
     deleteSubcorpus,
+    updateCorpusPreppedStatus,
     getFileText,
 } = require('../ServerControllers/corpusManagerController.js');
 
@@ -29,6 +30,7 @@ router.patch('/groups/:groupId', patchCorpusGroupName); // edit a group name ass
 router.post('/groups/:groupId/file', uploadFileContent); // add a file associated with a particular group id.
 router.delete('/files/:fileId', deleteCorpusFile); // delete a file given its ID. Will also delete other data associated with it.
 router.delete('/groups/:groupId', deleteSubcorpus); // delete a corpus group given its ID. Will also delete other data associated with the subcorpus.
+router.post('/summarizer/:corpusId', updateCorpusPreppedStatus); // update the prepped status of a corpus - i.e., does the summary word count need updating or not?
 router.get('/text/:fileId', getFileText);
 
 module.exports = router;
