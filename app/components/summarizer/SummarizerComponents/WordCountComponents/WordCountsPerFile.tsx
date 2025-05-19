2306e686f45f97387a913a8c4f2fce0278e6c407
Daniel Parsons <parsonsdwp@gmail.com>
2025-05-19 13:57:13 +0900

    chore: establish word count components

---
+++ app/components/summarizer/SummarizerComponents/WordCountComponents/WordCountsPerFile.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+// State management and context
+import { useState, useEffect } from 'react';
+import { useCorpusMetaData } from '@/app/context/CorpusContext';
+
+// CSS
+import '@/styles/summarizer.css';
+
+const WordCountsPerFile = () => {
+
+}
+
+export default WordCountsPerFile;
